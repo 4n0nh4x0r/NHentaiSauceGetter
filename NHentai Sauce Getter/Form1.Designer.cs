632ad408089cc1992d0d4f3f25b6470bbc1687ac
aiden@zuchtbude.de <aiden@zuchtbude.de>
2021-09-23 12:35:06 +0200

    Initial upload of the source code

---
+++ NHentai Sauce Getter/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace NHentai_Sauce_Getter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.sauceLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.tagsIncluded = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderLink = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderTitle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPages = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tagsExcluded = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(20, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(378, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get a random sauce";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sauceLinkLabel
+            // 
+            this.sauceLinkLabel.AutoSize = true;
+            this.sauceLinkLabel.Location = new System.Drawing.Point(12, 27);
+            this.sauceLinkLabel.Name = "sauceLinkLabel";
+            this.sauceLinkLabel.Size = new System.Drawing.Size(150, 13);
+            this.sauceLinkLabel.TabIndex = 2;
+            this.sauceLinkLabel.TabStop = true;
+            this.sauceLinkLabel.Text = "Select the sauce and click me";
+            this.sauceLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.sauceLinkLabel_LinkClicked);
+            // 
+            // tagsIncluded
+            // 
+            this.tagsIncluded.Location = new System.Drawing.Point(98, 24);
+            this.tagsIncluded.Name = "tagsIncluded";
+            this.tagsIncluded.Size = new System.Drawing.Size(300, 20);
+            this.tagsIncluded.TabIndex = 3;
+            this.tagsIncluded.Text = "fox-girl, tail";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Included Tags";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Controls.Add(this.sauceLinkLabel);
+            this.groupBox1.Location = new System.Drawing.Point(436, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 162);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(268, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Open link directly";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderLink,
+            this.columnHeaderTitle,
+            this.columnHeaderPages});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(347, 92);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeaderLink
+            // 
+            this.columnHeaderLink.Text = "Sauce";
+            this.columnHeaderLink.Width = 153;
+            // 
+            // columnHeaderTitle
+            // 
+            this.columnHeaderTitle.DisplayIndex = 2;
+            this.columnHeaderTitle.Text = "Title";
+            // 
+            // columnHeaderPages
+            // 
+            this.columnHeaderPages.DisplayIndex = 1;
+            this.columnHeaderPages.Text = "Pages";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tagsExcluded);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.tagsIncluded);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(418, 162);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Setup";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Excluded Tags";
+            // 
+            // tagsExcluded
+            // 
+            this.tagsExcluded.Location = new System.Drawing.Point(98, 66);
+            this.tagsExcluded.Name = "tagsExcluded";
+            this.tagsExcluded.Size = new System.Drawing.Size(300, 20);
+            this.tagsExcluded.TabIndex = 7;
+            this.tagsExcluded.Text = "rape, mind-break";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 183);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(689, 205);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "NHentai Random Sauce Getter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel sauceLinkLabel;
+        private System.Windows.Forms.TextBox tagsIncluded;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tagsExcluded;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderLink;
+        private System.Windows.Forms.ColumnHeader columnHeaderPages;
+        private System.Windows.Forms.ColumnHeader columnHeaderTitle;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ NHentai Sauce Getter/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NHentai_Sauce_Getter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        async private void getSauce()
+        {
+
+            string[] includedTags = tagsIncluded.Text.Split(',');
+            string[] excludedTags = tagsExcluded.Text.Split(',');
+
+            // There must be at least one tag in the include list
+            if(includedTags.Length == 1 && includedTags[0] == "")
+            {
+                MessageBox.Show("You need to have at least one tag in the include box", "Error");
+                return;
+            }
+
+            // Formatting the include taglist to have a tag followed by ", " (Inportant for the nhentai library to work)
+            string[] outputArray = new string[includedTags.Length];
+            for (int i = 0; i < includedTags.Length; i++)
+            {
+                outputArray[i] = NHentaiSharp.Core.SearchClient.GetExcludeTag(includedTags[i].Trim());
+            }
+            string outputStringIncludes = String.Join(", ", outputArray);
+
+
+            // Formatting the exclude taglist to have a tag followed by ", " (Inportant for the nhentai library to work)
+            outputArray = new string[excludedTags.Length];
+            for (int i = 0; i < excludedTags.Length; i++)
+            {
+                outputArray[i] = NHentaiSharp.Core.SearchClient.GetExcludeTag(excludedTags[i].Trim());
+            }
+            string outputStringExcludes = String.Join(", ", outputArray);
+
+            // Here we set the tags that we want to include and exclude from our search
+            string[] tags = new[] {
+                outputStringIncludes,
+                outputStringExcludes
+            };
+
+
+            Random r = new Random();
+            try
+            {
+                // We do a search with the tags
+                var result = await NHentaiSharp.Core.SearchClient.SearchWithTagsAsync(tags);
+                int page = r.Next(0, result.numPages) + 1; // Page count begin at 1
+                                                           // We do a new search at a random page
+                result = await NHentaiSharp.Core.SearchClient.SearchWithTagsAsync(tags, page);
+                var doujinshi = result.elements[r.Next(0, result.elements.Length)]; // We get a random doujinshi
+
+                // Here we add the doujin information to our listview box on the right side
+                listView1.Items.Add(new ListViewItem(new string[] { (doujinshi.id).ToString(), doujinshi.englishTitle, (doujinshi.numPages).ToString()}));
+            }
+            catch(Exception e)
+            {   
+                // In case no doujin is being found, the upper code will error, to prevent the program from crashing we do this catch
+                MessageBox.Show("No results could be found with these tags", "Search Error");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // We simply execude our function that handles the entire process to get the sauces
+            getSauce();
+        }
+
+        private void sauceLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Open link in browser
+            System.Diagnostics.Process.Start(sauceLinkLabel.Text);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (checkBox1.Checked)
+                {
+                    case true: // Open the link directly in the browser
+                        System.Diagnostics.Process.Start("https://www.nhentai.net/g/" + listView1.SelectedItems[0].Text.ToString());
+                        break;
+                    case false: // Change the linklabel on the form to the sauce for manual opening
+                        sauceLinkLabel.Text = "https://www.nhentai.net/g/" + listView1.SelectedItems[0].Text.ToString();
+                        break;
+                }
+            }catch (Exception err){
+                // An error can occur while trying to change the linklabel text, dont ask me why, just accept it and....whatever
+                Console.WriteLine(err);
+            }
+        }
+    }
+}
